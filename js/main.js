ebf8e939e4f1a92a5b8b637e271c42d6a2b47e65
puszk <puszka.kontakt@gmial.com>
2024-08-14 15:15:34 +0200

    contact section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-});
+// });
 
+// /**
+//  * @license
+//  * Copyright 2019 Google LLC. All Rights Reserved.
+//  * SPDX-License-Identifier: Apache-2.0
+//  */
+// // Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.querySelector(".contact__map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
