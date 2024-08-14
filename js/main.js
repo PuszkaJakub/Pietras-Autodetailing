84a75c4f90a669bba0fef5e17204f7418cc6e127
puszk <puszka.kontakt@gmial.com>
2024-08-14 21:02:27 +0200

    navigation and small corrects

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 const footerYear = document.querySelector('.footer__year');
+const nav = document.querySelector('.nav__mobile');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__mobile-item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
+
+const handleNav = () => {
+	nav.classList.toggle('nav__mobile--active');
+
+	allNavItems.forEach(element => {
+		element.addEventListener('click', () => {
+			nav.classList.remove('nav__mobile--active');
+		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+
 handleCurrentYear();
+navBtn.addEventListener('click', handleNav);
 
 // /**
 //  * @license
--- js/slick.js
+++ js/slick.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2500,
+		autoplaySpeed: 2000,
 		arrows: false,
 		mobileFirst: true,
 		responsive: [
